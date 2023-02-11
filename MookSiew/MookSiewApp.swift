f3cce4ec6e2359c1bfa680260c9d2f387ade7d26
Tyme Suteesopon <tyme.suteesopon@gmail.com>
2023-02-11 07:58:32 +0700

    Initial Commit

---
+++ MookSiew/MookSiewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MookSiewApp.swift
+//  MookSiew
+//
+//  Created by Tyme Suteesopon on 10/2/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MookSiewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
